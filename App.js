e667a8bff9ea4d7654218ff0f36340661d89e0be
91anita <45237162+91anita@users.noreply.github.com>
2023-04-20 10:25:40 +0530

    added about page

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css"
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import { Navbar } from "./components/navbar"; 
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+import { About } from "./pages/about/about";
+import { ShopContextProvider } from "./context/shop-contex";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <ShopContextProvider>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Shop/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+        </Routes>
+      </Router>
+      </ShopContextProvider>
+      
+    </div>
+    
+  );
+}
+
+export default App;
